fbb6b8599079f747865905d0f1bf2feb1d16f18d
bmaner <bmanerdaniel@gmail.com>
2021-12-11 16:54:06 +0900

    Learn Webpack

---
+++ dist/main.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = 'JavaScript study';
+
+    return element;
+}
+
+document.body.appendChild(component());
+
+/******/ })()
+;
